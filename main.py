0a0bf2aae90b0b601a0c0a59a8e83d4cc42cd3db
Haen <haensito@gmail.com>
2025-10-23 17:08:14 -0700

    "se agregaron los graficos, al menos unos pocos"

--- main.py
+++ main.py
@@ -6,39 +6,15 @@ import numpy as np
 
 base_datos=pd.read_excel(r"C:\Users\hankr\Downloads\Base_alumnos 3.xlsx")
 
-
-
-
-
-
-
-
-carrera = base_datos["carrera"].drop_duplicates().reset_index(drop=True)
-carrera_df = pd.DataFrame({"id_carrera": range(1, len(carrera) + 1),"carrera": carrera
-})
-
-
-preparatoria_df = base_datos[["escuela_procedencia", "nombre_preparatoria"]].drop_duplicates().reset_index(drop=True)
-
-
-
-
-
-#print(preparatoria_df)
-
-
-
 #Se crea la tabla alumnos con indice no_control
-alumnos_df=base_datos[["no_de_control","nombre_alumno", "sexo"]]
+alumnos_df=base_datos[["no_de_control","nombre_alumno", "sexo","ficha"]]
 #alumnos_df = alumnos_df.set_index("no_de_control")
 
-alumnos_df['id_carrera_fk'] = np.nan
 
 
-#print(alumnos_df)
+
 
 xlsx_cpa_mat_diag=pd.read_excel(r"C:\Users\hankr\Downloads\2025 CPA MAT COMP diag.xlsx")
-
 
 
 # Renombrar la columna en xlsx_cpa_mat_diag para que coincida con 'ficha'
@@ -61,9 +37,6 @@ examen_diag_df = pd.concat([merged_diag_df["ficha"].reset_index(drop=True), preg
 
 
 xlsx_cpa_mat_final=pd.read_excel(r"C:\Users\hankr\Downloads\2025 CPA mat final.xlsx")
-
-
-
 
 
 cols_a_combinar = ["NUMERO DE CONTROL.1", "Unnamed: 26", "Unnamed: 27", "Unnamed: 28"]  # o cualquier lista de columnas
@@ -109,8 +82,13 @@ plt.tight_layout()
 
 # Mostrar o guardar el gráfico
 # plt.show() # Si estás en un entorno interactivo
-plt.savefig('conteo_alumnos_por_escuela.png')
-plt.show()
+#plt.savefig('conteo_alumnos_por_escuela.png')
+#plt.show()
+
+
+
+
+print(alumnos_df)
 
 
 
